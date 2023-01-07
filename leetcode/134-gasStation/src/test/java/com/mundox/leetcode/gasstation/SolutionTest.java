4fe5c5d0a0f9fa858cf2f9f9fb7a32750fbafe1c
Milenio X <jnkmiloq@gmail.com>
2023-01-07 12:55:38 -0500

    Initial project structure

---
+++ leetcode/134-gasStation/src/test/java/com/mundox/leetcode/gasstation/SolutionTest.java
@@ -0,0 +1,11 @@
+package com.mundox.leetcode.gasstation;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+  @Test
+  public void test1() {
+     assertEquals(true, true);
+  }
+}
